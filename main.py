d4615cfe5a8533c6058830de7777bec381b6ba2a
Smorchkov Misha <mishutka200101@mail.ru>
2022-06-15 00:11:20 +0300

    new commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests as r
+
+
+def main():
+    with open('accounts.txt', 'r') as file:
+        accounts = file.read().split('\n')
+
+    for account in accounts:
+        res = r.post(url="https://hook.us1.make.com/as22p9q6fmnemmxku4cq927jwplsyi92", json={"EMAIL": account, "PAGE_AND_SECTION": "https://forms.leapwallet.io/leap-cosmos#cosmos-emailcapture-1"})
+        print(f"{account} registered")
+
+
+if __name__ == "__main__":
+    main()
